7be70014f98c6a099cb215975664acd85894ac37
MuniMunii <tyuimiti@gmail.com>
2025-08-19 23:15:21 +0700

    fixing typo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -64,7 +64,7 @@ export default function Navbar() {
           }`}
           to={"/"}
         >
-          Beranda
+          Home
         </Link>
         <Link
           className={`text-black ${
